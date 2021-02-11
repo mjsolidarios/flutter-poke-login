7a656e87c4d995fa4fae0519d4df57ce0543afb3
mjsolidarios <bashdev7@gmail.com>
2021-02-11 18:20:09 +0800

    Additional animations

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -25,12 +25,17 @@ class _LoginState extends State<Login> {
   final riveFileName = 'assets/pikachu.riv';
   Artboard _artboard;
   RiveAnimationController _controller;
-  CustomAnimator _tail_animation_controller;
-  bool _tail_animate = false;
+  CustomAnimator _tailController = CustomAnimator('tail-wag');
+  CustomAnimator _lookController = CustomAnimator('look-down');
+  CustomAnimator _idleController = CustomAnimator('idle');
+  CustomAnimator _earsUpController = CustomAnimator('ears-up');
+  CustomAnimator _hideEyesController = CustomAnimator('hide-eyes');
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
     _loadRiveFile();
+
     super.initState();
   }
 
@@ -40,59 +45,162 @@ class _LoginState extends State<Login> {
     final file = RiveFile();
 
     if (file.import(bytes)) {
-      // Select an animation by its name
-      setState(() => _artboard = file.mainArtboard
-        ..addController(
-          SimpleAnimation('idle'),
-        ));
+      Artboard baseArtboard = file.mainArtboard;
+      baseArtboard.addController(_tailController);
+      _tailController.stop();
+      baseArtboard.addController(_lookController);
+      baseArtboard.addController(_idleController);
+      _idleController.stop();
+      baseArtboard.addController(_earsUpController);
+      setState(() => _artboard = baseArtboard);
     } else {
       print("Error loading file.");
     }
   }
 
-  void _wipersChange(bool tailOn) {
-    if (_tail_animation_controller == null) {
-      _artboard.addController(
-        _tail_animation_controller = CustomAnimator('tail-wag'),
-      );
-    }
-    if (tailOn) {
-      _tail_animation_controller.start();
+  void _hideEyes(bool on) {
+    if (on) {
+      _hideEyesController.start();
     } else {
-      _tail_animation_controller.stop();
+      _hideEyesController.stop();
     }
-    setState(() => _tail_animate = !tailOn);
+  }
+
+  void _wagTail(bool on) {
+    if (on) {
+      _tailController.start();
+    } else {
+      _tailController.stop();
+    }
+  }
+
+  void _lookOnEmail(bool on) {
+    if (on) {
+      _lookController.start();
+    } else {
+      _lookController.stop();
+    }
+  }
+
+  void _earsUp(bool on) {
+    if (on) {
+      _earsUpController.start();
+    } else {
+      _earsUpController.stop();
+    }
+  }
+
+  void _idle(bool on) {
+    if (on) {
+      _idleController.start();
+    } else {
+      _idleController.stop();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     double screen_width = MediaQuery.of(context).size.width;
+    double screen_height = MediaQuery.of(context).size.height;
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Login"),
         ),
-        body: Container(
-            child: Column(
-          children: [
-            Center(
-              child: Container(
-                width: screen_width - 10,
-                height: screen_width - 10,
-                child: _artboard != null
-                    ? Rive(
-                        artboard: _artboard,
-                        fit: BoxFit.cover,
-                      )
-                    : Text("data"),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                color: Colors.white,
+                width: screen_width,
+                height: screen_height,
               ),
-            ),
-            MaterialButton(
-                color: Colors.yellow,
-                child: Text('Press to Animate Tail'),
-                onPressed: () {
-                  _wipersChange(_tail_animate);
-                })
-          ],
-        )));
+              Container(
+                color: Colors.lightBlue,
+                width: screen_width,
+                height: screen_height * 0.55,
+              ),
+              Column(
+                children: [
+                  Center(
+                    child: Container(
+                      width: screen_width - 10,
+                      height: screen_width - 10,
+                      child: _artboard != null
+                          ? Rive(
+                              artboard: _artboard,
+                              fit: BoxFit.cover,
+                            )
+                          : Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                  top: screen_height * 0.45,
+                  child: Container(
+                    width: screen_width,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          topRight: Radius.circular(20)),
+                      color: Colors.white,
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          Form(
+                              key: _formKey,
+                              child: Column(children: <Widget>[
+                                FocusScope(
+                                  onFocusChange: (value) {
+                                    if (!value) {
+                                      _idle(true);
+                                      _lookController.apply(_artboard, -1);
+                                    } else {
+                                      _lookOnEmail(true);
+                                      _hideEyesController.apply(_artboard, -1);
+                                    }
+                                  },
+                                  child: TextFormField(
+                                    decoration:
+                                        InputDecoration(labelText: "Email"),
+                                  ),
+                                ),
+                                FocusScope(
+                                  onFocusChange: (value) {
+                                    if (!value) {
+                                      _wagTail(true);
+                                    } else {
+                                      _idle(false);
+                                    }
+                                  },
+                                  child: TextFormField(
+                                    obscureText: true,
+                                    decoration:
+                                        InputDecoration(labelText: "Password"),
+                                  ),
+                                ),
+                              ])),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          MaterialButton(
+                              minWidth: screen_width,
+                              color: Colors.yellow,
+                              child: Text('Login'),
+                              onPressed: () {
+                                _hideEyes(true);
+                              })
+                        ],
+                      ),
+                    ),
+                  ))
+            ],
+          ),
+        ));
   }
 }
